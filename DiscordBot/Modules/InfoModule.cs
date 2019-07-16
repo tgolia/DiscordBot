486c89431d83f55937bf4f930f85b0d2b06bd6da
rohhan <rohhan@gmail.com>
2019-07-15 22:05:33 -0700

    Added a sample command module

---
+++ DiscordBot/Modules/InfoModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        => ReplyAsync(echo);
+    }
+}

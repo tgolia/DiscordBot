664a0be3999f12ff1b1f9aa61a55ed5c40a2f081
Ro Ko <rohhan@gmail.com>
2019-07-20 12:21:23 -0700

    Added initial migration.

---
+++ DiscordBot/Modules/AdminModule.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Modules
+{
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+    }
+}

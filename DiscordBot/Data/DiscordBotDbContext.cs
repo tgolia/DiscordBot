79cda3384ad5b90802e7be72b135d9b76f84c09d
Travis Golia <travis@gigtown.com>
2019-07-20 11:31:33 -0700

    Set up many to many relationship betw Users-Guilds

---
+++ DiscordBot/Data/DiscordBotDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DiscordBot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordBot.Data
+{
+    public class DiscordBotDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Guild> Guilds { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GuildUser>()
+                .HasKey(g => new { g.GuildId, g.UserId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = DiscordBotData; Trusted_Connection = True; ");
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}

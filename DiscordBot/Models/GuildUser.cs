d08295e4e4336bc308fdca0c72bb81390f9b06b4
rohhan <rohhan@gmail.com>
2019-08-18 11:38:52 -0700

    Added guild name and guild date created

--- DiscordBot/Models/GuildUser.cs
+++ DiscordBot/Models/GuildUser.cs
@@ -4,8 +4,11 @@ namespace DiscordBot.Models
     public class GuildUser
     {
         public int UserId { get; set; }
+
         public int GuildId { get; set; }
+
         public User User { get; set; }
+
         public Guild Guild { get; set; }
     }
 }
